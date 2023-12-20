2d97ac8fbb2dcb0361c7e7729ca4c9d3981b682d
mohmlll <mohammed.malloul@hotmail.com>
2023-12-20 16:48:14 +0100

    ci: remove SonarQube for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,4 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id ("org.sonarqube") version "4.4.1.3373"
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "test")
-        property("sonar.projectName", "Capstone")
-        property("sonar.qualitygate.wait", true)
-        property("sonar.gradle.skipCompile", true)
-    }
 }
