adec82964eee1013159982efebf3468293c6d8b8
Malloum003 <mohammed.malloul@hva.nl>
2023-12-23 18:36:54 +0100

    feat(Home): update Home screen

--- app/src/main/java/mohammed/capstone/ui/screens/HomeScreen.kt
+++ app/src/main/java/mohammed/capstone/ui/screens/HomeScreen.kt
@@ -1,31 +1,235 @@
 package mohammed.capstone.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import kotlinx.coroutines.delay
+import mohammed.capstone.R
 import mohammed.capstone.ui.theme.CapstoneTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
+@ExperimentalMaterialApi
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    CapstoneTheme(darkTheme = true) {
+    val focusManager = LocalFocusManager.current
+    val state = rememberPullToRefreshState() { true }
 
-        Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-            Text(
-                text = "Welcome to the Home Screen!",
-                style = MaterialTheme.typography.bodyLarge,
+    if (state.isRefreshing) {
+        LaunchedEffect(true) {
+            delay(1500)
+            // TODO: Refresh state
+            state.endRefresh()
+        }
+    }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .nestedScroll(state.nestedScrollConnection)) {
+        LazyColumn(
+            modifier = Modifier.padding(16.dp)
+        ) {
+
+            item {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Home Page Banner",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clip(shape = MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.Crop,
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                IntroductionSection()
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                if (!state.isRefreshing) {
+                    FeaturedProjectsSection(navController)
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    ContactMeSection(focusManager)
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                }
+            }
+        }
+
+        PullToRefreshContainer(
+            modifier = Modifier.align(Alignment.TopCenter),
+            state = state,
+        )
+    }
+}
+
+
+@Composable
+private fun IntroductionSection() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello, I'm John Doe",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        Text(
+            text = "Software Engineer | Mobile App Developer",
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
+
+@Composable
+private fun FeaturedProjectsSection(navController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Featured Projects",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        FeaturedProjectCard(
+            title = "Project 1",
+            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            onClick = { navController.navigate("project1") }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        FeaturedProjectCard(
+            title = "Project 2",
+            description = "Suspendisse non dui eget arcu hendrerit posuere.",
+            onClick = { navController.navigate("project2") }
+        )
+    }
+}
+
+@Composable
+private fun ContactMeSection(focusManager: FocusManager) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Contact Me",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        BasicTextField(
+            value = remember { mutableStateOf("") }.value,
+            onValueChange = { /* handle input */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface)
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = MaterialTheme.shapes.small
+                )
+                .padding(horizontal = 16.dp, vertical = 16.dp)
+                .height(120.dp),
+            textStyle = MaterialTheme.typography.bodySmall.copy(
                 color = MaterialTheme.colorScheme.onPrimary
+            ),
+            singleLine = false,
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                }
+            )
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Button(
+                onClick = { /* Handle button click */ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            ) {
+                Text(text = "Contact")
+            }
+        }
+    }
+}
+
+@Composable
+fun FeaturedProjectCard(
+    title: String,
+    description: String,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
             )
         }
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
