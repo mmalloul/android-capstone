1147e7e23b2b740d5b9922f52cf377a37577df41
Malloum003 <mohammed.malloul@hva.nl>
2024-01-12 05:18:48 +0100

    refactor: moved loadindicator to utils

--- app/src/main/java/mohammed/capstone/utils/Utils.kt
+++ app/src/main/java/mohammed/capstone/utils/Utils.kt
@@ -2,15 +2,30 @@ package mohammed.capstone.utils
 
 import android.content.Context
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.core.net.toUri
 
 object Utils {
+    const val REFRESH_TIME: Long = 1500
     fun openCustomTab(context: Context, url: String) {
         try {
             val customTabsIntent = CustomTabsIntent.Builder().build()
             customTabsIntent.launchUrl(context, url.toUri())
-        } catch (e: Exception) {
-            // Handle exceptions such as malformed URLs
+        } catch (_: Exception) {
+
+        }
+    }
+
+    @Composable
+    fun LoadingIndicator() {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
         }
     }
 }
